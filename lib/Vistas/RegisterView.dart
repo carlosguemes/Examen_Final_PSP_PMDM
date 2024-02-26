3f1f4728760b75ccf3ffd30e6556c0143bb5c534
carlos <carlosguemesdam@gmail.com>
2024-02-26 19:07:29 +0100

    Commit V4: Añadiendo Textos a RegisterView

--- lib/Vistas/RegisterView.dart
+++ lib/Vistas/RegisterView.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 class RegisterView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: const Text('Registro'),
+      ),
+
+      body: Column(children: [
+        Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+        Text('Identifíquese como nuevo usuario', style: TextStyle(fontSize: 25)),
+        Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+
+        ]
+
+      ),
+    );
   }
   
 }
